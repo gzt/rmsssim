f00505c14e886b2ae680694113edb94a7e68617e
gzt <gzthompson@gmail.com>
2018-12-14 02:25:16 -0600

    tests

--- tests/testthat/test-example.R
+++ tests/testthat/test-example.R
@@ -8,11 +8,16 @@ y <- matrix(y, 128)
 x <- raster::raster(x)
 y <- raster::raster(y)
 #tmp <- tempfile()
+test_that("MSSims should be equal", {
 expect_equal(msssim(x,x,5),1, tolerance = .001)
 expect_equal(msssim(y,y,5),1, tolerance = .001)
 expect_equal(msssim(x,y,5),msssim(y,x,5))
+})
+
 #expect_known_value(msssim(x,y,5), tmp)
+test_that("SSIMs should be equal:", {
 expect_equal(ssim(x,x,5)[[1]],1, tolerance = .001)
 expect_equal(ssim(y,y,5)[[1]],1, tolerance = .001)
 expect_equal(ssim(x,y,5)[[1]],ssim(y,x,5)[[1]])
 #expect_equal_to_reference(ssim(x,y,5))
+})
