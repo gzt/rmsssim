2288323f56fba9c968f6772639138faad1056f31
gzt <gzthompson@gmail.com>
2018-12-14 02:39:37 -0600

    add tolerance

--- tests/testthat/test-example.R
+++ tests/testthat/test-example.R
@@ -11,13 +11,13 @@ y <- raster::raster(y)
 test_that("MSSims should be equal", {
 expect_equal(msssim(x,x,5),1, tolerance = .001)
 expect_equal(msssim(y,y,5),1, tolerance = .001)
-expect_equal(msssim(x,y,5),msssim(y,x,5))
+expect_equal(msssim(x,y,5),msssim(y,x,5), tolerance = .001)
 })
 
 #expect_known_value(msssim(x,y,5), tmp)
 test_that("SSIMs should be equal:", {
 expect_equal(ssim(x,x,5)[[1]],1, tolerance = .001)
 expect_equal(ssim(y,y,5)[[1]],1, tolerance = .001)
-expect_equal(ssim(x,y,5)[[1]],ssim(y,x,5)[[1]])
+expect_equal(ssim(x,y,5)[[1]],ssim(y,x,5)[[1]], tolerance = .001)
 #expect_equal_to_reference(ssim(x,y,5))
 })
